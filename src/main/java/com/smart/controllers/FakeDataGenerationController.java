951a73d7e3b695815a19a1b7318b6be7eb4e953c
Devendra6536 <devendrapal8682@gmail.com>
2024-02-16 15:10:26 +0530

    changed

--- src/main/java/com/smart/controllers/FakeDataGenerationController.java
+++ src/main/java/com/smart/controllers/FakeDataGenerationController.java
@@ -5,19 +5,14 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.smart.dao.FakeUserRepository;
 import com.smart.dao.TestDataRepository;
-import com.smart.entities.FakeUser;
 import com.smart.entities.TestData;
 import com.smart.entities.TestMetaDataInformation;
 
